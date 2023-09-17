0ab340c7400c4bd0fd7c1f2c56527cfdae24ed17
vlysetskyi <vlysetskyi@dedicatted.com>
2023-09-18 00:03:02 +0300

    finalized lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,40 +1,84 @@
-# Define the alphabet table
-alphabet_table = {
-    'А': '01', 'Б': '02', 'В': '03', 'Г': '04', 'Ґ': '05',
-    'Д': '06', 'Е': '07', 'Є': '08', 'Ж': '09', 'З': '10',
-    'И': '11', 'І': '12', 'Ї': '13', 'Й': '14', 'К': '15',
-    'Л': '16', 'М': '17', 'Н': '18', 'О': '19', 'П': '20',
-    'Р': '21', 'С': '22', 'Т': '23', 'У': '24', 'Ф': '25',
-    'Х': '26', 'Ц': '27', 'Ч': '28', 'Ш': '29', 'Щ': '30',
-    'Ь': '31', 'Ю': '32', 'Я': '33', ' ': '34', '0': '35',
-    '1': '36', '2': '37', '3': '38', '4': '39', '5': '40',
-    '6': '41', '7': '42', '8': '43', '9': '44'
+ALPHABET_TABLE = {
+    'А': 1, 'Б': 2, 'В': 3, 'Г': 4, 'Ґ': 5,
+    'Д': 6, 'Е': 7, 'Є': 8, 'Ж': 9, 'З': 10,
+    'И': 11, 'І': 12, 'Ї': 13, 'Й': 14, 'К': 15,
+    'Л': 16, 'М': 17, 'Н': 18, 'О': 19, 'П': 20,
+    'Р': 21, 'С': 22, 'Т': 23, 'У': 24, 'Ф': 25,
+    'Х': 26, 'Ц': 27, 'Ч': 28, 'Ш': 29, 'Щ': 30,
+    'Ь': 31, 'Ю': 32, 'Я': 33, '_': 34, '0': 35,
+    '1': 36, '2': 37, '3': 38, '4': 39, '5': 40,
+    '6': 41, '7': 42, '8': 43, '9': 44
 }
 
-# Define the gamma key
-gamma_key = "БОРИМИР"
+def initialize_alphabet_table():
+    return ALPHABET_TABLE
 
-# Convert the gamma key to numerical values
-gamma_numerical = [alphabet_table[letter] for letter in gamma_key]
+def convert_gamma_to_numerical(gamma_key):
+    alphabet_table = initialize_alphabet_table()
+    return [alphabet_table.get(char, 0) for char in gamma_key]
 
-# Message to encrypt
-message = "ЧОРАПНЕМОРЕ_201"
+def numerical_to_string(numbers):
+    alphabet_table = initialize_alphabet_table()
+    return ''.join([char for num in numbers for char, code in alphabet_table.items() if code == num])
 
-# Initialize the encrypted message
-encrypted_message = ""
+def encrypt(message, gamma_key):
+    alphabet_table = initialize_alphabet_table()
+    message_numbers = [alphabet_table.get(char, 0) for char in message]
+    gamma_key_numbers = convert_gamma_to_numerical(gamma_key)
 
-# Encrypt the message using the gamma key
-for i, char in enumerate(message):
-    if char in alphabet_table:
-        char_code = alphabet_table[char]
-        gamma_code = gamma_numerical[i % len(gamma_key)]
-        encrypted_char_code = (int(char_code) + int(gamma_code)) % 45  # Modulo 45 to wrap around the alphabet
-        for letter, code in alphabet_table.items():
-            if code == str(encrypted_char_code).zfill(2):
-                encrypted_message += letter
-    else:
-        # If the character is not in the alphabet table, keep it unchanged
-        encrypted_message += char
+    encrypted_numbers = []
+    for i, message_num in enumerate(message_numbers):
+        gamma_index = i % len(gamma_key_numbers)
+        sum_result = message_num + gamma_key_numbers[gamma_index]
+        encrypted_number = sum_result % len(alphabet_table)
 
-# Print the encrypted message
-print("Encrypted Message:", encrypted_message)
+        encrypted_numbers.append(encrypted_number)
+
+    encrypted_string = numerical_to_string(encrypted_numbers)
+    return encrypted_string
+
+def decrypt(encrypted_message, gamma_key):
+    alphabet_table = initialize_alphabet_table()
+    encrypted_numbers = convert_gamma_to_numerical(encrypted_message)
+    gamma_key_numbers = convert_gamma_to_numerical(gamma_key)
+
+    decrypted_numbers = []
+    for i, encrypted_num in enumerate(encrypted_numbers):
+        gamma_index = i % len(gamma_key_numbers)
+        diff_result = encrypted_num - gamma_key_numbers[gamma_index]
+
+        if diff_result < 0:
+            diff_result += len(alphabet_table)
+
+        decrypted_numbers.append(diff_result)
+
+    decrypted_string = numerical_to_string(decrypted_numbers)
+    return decrypted_string
+
+def task1(gamma_key, message):
+    encrypted_message = encrypt(message, gamma_key)
+    
+    print("Gamma key:", gamma_key)
+    print("Message:", message)
+    print("Encrypted Message:", encrypted_message)
+    return 1
+    
+def task2(gamma_key, encrypted_message):
+    decrypted_message = decrypt(encrypted_message, gamma_key)
+    print("Gamma key:", gamma_key)
+    print("Encrypted Message:", encrypted_message)
+    print("Decrypted Message:", decrypted_message)
+    return 1
+def main():
+    print("================================================")
+    print("Завдання 1")
+    task1("БОРИМИР", "ЧОРАПНЕМОРЕ_201")
+    print("================================================")
+    print()
+    print("================================================")
+    print("Завдання 2")
+    task2("ДИВОЗІР", "ДЗЧЦ_РУ6ЇЗЇЕУНРЮ")
+    print("================================================")
+
+if __name__ == "__main__":
+    main()
