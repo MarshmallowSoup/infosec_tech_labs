a40832530b7237e609c755338ad800fb7e953ce5
Volodymyr Lysetskyi <vlysetskyi@dedicatted.com>
2023-10-23 14:41:08 +0300

    fixed RSA in lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,3 +1,5 @@
+import math
+
 ALPHABET_TABLE = {
     'А': 1, 'Б': 2, 'В': 3, 'Г': 4, 'Ґ': 5,
     'Д': 6, 'Е': 7, 'Є': 8, 'Ж': 9, 'З': 10,
@@ -13,9 +15,9 @@ ALPHABET_TABLE = {
 def initialize_alphabet_table():
     return ALPHABET_TABLE
 
-def convert_to_numerical(gamma_key):
+def convert_to_numerical(text):
     alphabet_table = initialize_alphabet_table()
-    return [alphabet_table.get(char, 0) for char in gamma_key]
+    return [alphabet_table.get(char, 0) for char in text]
 
 def numerical_to_string(numbers):
     alphabet_table = initialize_alphabet_table()
@@ -24,4 +26,42 @@ def numerical_to_string(numbers):
 def generate_rsa_keys(p, q):
     # Обчислення n та φ(n)
     n = p * q
+    print("n =", n)
     phi_n = (p - 1) * (q - 1)
+    return phi_n, n
+
+def encrypt(text, n, phi):
+    print(f'Original message:{text}')
+    msg = convert_to_numerical(text)
+    e = 2
+    while(e < phi):
+        if (math.gcd(e, phi) == 1):
+            break
+        else:
+            e += 1
+    print("e =", e)
+
+    encrypted_msg = [pow(char, e) % n for char in msg]
+    print(f'Encrypted message: {encrypted_msg}')
+    return encrypted_msg, e
+
+def decrypt(encrypted_msg, e, n, phi):
+    # Calculate the modular multiplicative inverse of e modulo phi
+    d = pow(e, -1, phi)
+    print("d =", d)
+    print(f'Public key: {e, n}')
+    print(f'Private key: {d, n}')
+    
+    # Decryption
+    decrypted_msg_num = [pow(char, d, n) for char in encrypted_msg]
+    decrypted_msg = numerical_to_string(decrypted_msg_num)
+    print(f'Decrypted message: {decrypted_msg}')
+    return decrypted_msg
+     
+msg = "ПЕНЯ15"
+p, q = 23, 41
+
+phi, n = generate_rsa_keys(p,q)
+
+encrypted_msg, e = encrypt(msg, n, phi)
+decrypted_msg = decrypt(encrypted_msg, e, n, phi)
